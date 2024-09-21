6f2a8657e6876e01787341a7c1de0c73f6dd6fee
Marileia <marileia.direito@gmail.com>
2024-09-21 14:16:14 -0300

    Primeiro commit

--- src/ConversaoBitsParaBytes.java
+++ src/ConversaoBitsParaBytes.java
@@ -1,2 +1,29 @@
-public class Exercicio3 {
+import java.util.Scanner;
+
+public class ConversaoBitsParaBytes {
+
+    public static final int BITS_EM_UM_BYTE = 8;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de bits (long): ");
+        long numeroDeBits = scanner.nextLong();
+
+        if (numeroDeBits >= 0 && numeroDeBits <= Long.MAX_VALUE) {
+
+            if (numeroDeBits % BITS_EM_UM_BYTE == 0) {
+               
+                long numeroDeBytes = numeroDeBits / BITS_EM_UM_BYTE;
+                System.out.println("Número de bytes: " + numeroDeBytes);
+            } else {
+                System.out.println("O número de bits deve ser múltiplo de " + BITS_EM_UM_BYTE + ".");
+            }
+        } else {
+            System.out.println("Por favor, insira um número de bits válido (positivo e até " + Long.MAX_VALUE + ").");
+        }
+
+        scanner.close();
+    }
 }
+
--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import javax.swing.JOptionPane;
 
-public class Exercico2 {
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        double total = 0;
+        int qtdNotas = 3;
+
+        for (int i = 1; i <= qtdNotas; i++) {
+            String input = JOptionPane.showInputDialog("Digite a nota " + i + ":");
+            if (input == null || input.isEmpty() || !input.matches("-?\\d+(\\.\\d+)?")) {
+                JOptionPane.showMessageDialog(null, "Entrada inválida. Programa será encerrado.");
+                System.exit(0);
+            }
+            total += Double.parseDouble(input);
+        }
+        JOptionPane.showMessageDialog(null, String.format("A média das notas é: %.2f", total / qtdNotas));
+    }
 }
